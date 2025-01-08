ef12baed098d192307d88d16d5017d555b67ecdd
nurassylmuktaruly <nuratop40@gmail.com>
2025-01-08 13:42:33 +0500

    create person class and add subclasses

---
+++ MyApplication.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MyApplication {
+    public static void main(String[] args) {
+        // Қызметкерлер мен студенттерді жасау
+        Employee emp1 = new Employee("John", "Lennon", "Manager", 27045.78);
+        Employee emp2 = new Employee("George", "Harrison", "Engineer", 50000.00);
+        Student stu1 = new Student("Ringo", "Starr", 2.5);
+        Student stu2 = new Student("Paul", "McCartney", 3.2);
+
+        ArrayList<Person> list = new ArrayList<>();
+        list.add(emp1);
+        list.add(emp2);
+        list.add(stu1);
+        list.add(stu2);
+
+        Collections.sort(list);
+
+        printData(list);
+    }
+
+    public static void printData(Iterable<? extends Payable> payables) {
+        for (Payable p : payables) {
+            System.out.printf("%s earns %.2f tenge%n", p.toString(), p.getPaymentAmount());
+        }
+    }
+}
--- Person.java
+++ Person.java
@@ -1,2 +1,59 @@
-public class Person {
+public class Person implements Payable, Comparable<Person> {
+    private static int count = 0;
+    private final int id;
+    private String name;
+    private String surname;
+
+    // Әдепкі конструктор
+    public Person() {
+        this.id = ++count;
+        this.name = "Unknown";
+        this.surname = "Unknown";
+    }
+
+    // Параметрлі конструктор
+    public Person(String name, String surname) {
+        this.id = ++count;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPosition() {
+        return "Student";
+    }
+
+    @Override
+    public String toString() {
+        return id + ". " + name + " " + surname;
+    }
+
+    @Override
+    public double getPaymentAmount() {
+        return 0.0; // Әдепкі төлем
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return Double.compare(this.getPaymentAmount(), other.getPaymentAmount());
+    }
 }
